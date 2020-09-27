34bcba43778088f051a04d38e3a5f92af4871716
coolerstuff10 <emmahmcardle@gmail.com>
2020-09-27 16:14:17 -0400

    First commit

---
+++ linked_list.rb
@@ -0,0 +1,19 @@
+class LinkedList
+
+  def append(value)
+    if @head == nil
+      prepend(value)
+    else
+    temp = head
+    while(tmp.next != nil) 
+    end
+  end
+
+  def prepend(value)
+    head = new Node(value, head)
+  end
+
+  def size
+    return 
+
+end
